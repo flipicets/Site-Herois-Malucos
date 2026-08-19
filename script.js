f03a480fa2119837c091821e95df4c4bf214c9fa
flipicets <fellipepse@gmail.com>
2026-08-19 20:53:59 -0300

    aodjufchn

---
+++ script.js
@@ -0,0 +1,6 @@
+const herois = [
+    { nome: "Super-Rúbia", universo: "nosso", poder: "forte", ano: 2007},
+    { nome: "fefoman", universo: "nosso", poder: "fofinho", ano: 2002},
+    { nome: "ursão", universo: "disney", poder: "urso", ano: 2026}
+    { nome: "MEGA GALÊ GIGANTAMAX", universo: "pokemon", poder: "gordola", ano: 2024}
+]
